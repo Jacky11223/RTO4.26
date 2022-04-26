d13a2ea85cb59d4a1c3e9cae0c29aab2bf2b65b4
Jacky11223 <103395254+Jacky11223@users.noreply.github.com>
2022-04-26 19:20:32 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+/* Applying the "open" class when you click the Menu button, which triggers the mobile menu to open/close; Toggle Menu/Close menu text. */
+$(document).ready(function() {
+  $('.mobile-button a').click(function(e) {
+	$(this).parent().parent().toggleClass('open');
+  $(this).html($(this).html() == 'Close Menu' ? 'Menu' : 'Close Menu');
+    e.preventDefault();
+  });
+});
+
+var myIndex = 0;
+  carousel();
+  
+  function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+  }
